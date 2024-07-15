6fe332c9c3584689e2f5a69e27c64f816d5bad2d
joel tech <joeljamestech3@gmail.com>
2024-07-15 17:37:59 +0300

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "test", reaction: "📎", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "test", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Hello my name is  *joel md * \n\n ' + "i'm a whatsapp bot multi-device created ";
-    let d = ' by *joel kang'oma*';
+    let z = 'Hello my name is  *joel md* \n\n ' + "i'm a whatsapp bot multi-device";
+    let d = ' by *joeltech*';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/bc9b68da99a9691e42ce8.jpg';
+    var img = 'https://telegra.ph/file/e07a3d933fb4cad0b3791.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -19,9 +19,9 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *Hacking* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *Thomas*'
+   let z ='Salut je m\'appelle *Zokou* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Djalega++*'
       let varmess=z+d
-      var img='https://telegra.ph/file/8c38114b4c25a63fa201a.jpg'
+      var img='https://telegra.ph/file/626e7105422c8908f723d.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */ 
