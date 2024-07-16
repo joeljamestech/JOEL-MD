9eda59baefc9d043113473ac016fba61f159eecf
joel tech <joeljamestech3@gmail.com>
2024-07-16 17:26:11 +0300

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -1,1 +1,194 @@
-function _0x1a77(){const _0x43e061=['MODE','private','2344BRSpLQ','\x0a|❏│\x20\x20\x20_Développer_\x20:\x20 joel \x0a|❏│\x20\x20&\x20best coder\x0a|❏|\x20\x20\x20\x20𝘃𝗲𝗿𝘀𝗶𝗼𝗻\x20\x20\x20\x20:\x20𝘝7.2\x0a\x20\x20\x20\x20╰───────────────\x0a╰─────✧JOEL-MD✧─────◆\x20\x0a\x0a','\x20👋\x0a\x0a*Liste\x20des\x20commandes\x20de\x20 JOEL-MD:*\x0a◇\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20◇\x0a','match','totalmem','util','3464630LvPpqo','19593OcBldp','18279984JngYoe','\x0a|❏│\x20\x20\x20_Commands_\x20:\x20','/../set','\x0a╰═════════════⊷\x0a','/../framework/mesfonctions','\x22\x20n\x27existe\x20pas.\x0a','\x0a|❏│\x20\x20\x20_Mémoire_\x20:\x20','\x0a|❏│\x20\x20\x20_Mode_\x20:\x20','Je\x20suis\x20*JOEL-MD*,\x20développé\x20par\x20joel+','🥵🥵\x20Menu\x20erreur\x20','fs-extra','15048429UWsrag','yes','toLocaleLowerCase','\x0a👋\x20Hello\x20how\x20are\x20you\x20','push','format','categorie','72002YVWoRz','\x0a*|❏│\x20','\x20tu\x20like\x20et\x20la\x20commande_nom.\x22\x0a\x20\x0a\x20\x20\x20\x20*powered by\x20by\x20joel-tech*\x20\x0a\x20\x20\x20joel\x20james\x20tech\x20wachannel:\x22https://whatsapp.com/channel/0029Vade9VgD38CPEnxfYF0M\x22\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a*»»—————\x20\x20\x20\x20★\x20\x20\x20—————««*\x0a','log','319kkKJTs','╭────🕷️','\x0a|❏│\x20\x20\x20_Plateforme_\x20:\x20','platform','\x0a╰═════════════⊷\x20\x0a','/../framework//zokou','length','4912aEYzuS','BOT','HH:mm:ss','✧────◆\x0aJOEL-MD\x20BOT\x20NEW\x20VERSION\x0a\x20\x20\x20\x20\x20╭──────────────\x0a|❏│\x20\x20\x20_Préfix_\x20:\x20','6lRWWaO','La\x20catégorie\x20\x22','menu','setDefault','276otkXyW','General','nomCom','/../framework/zokou','10klPWyR','\x20❏\x20✧────','OWNER_NAME','sendMessage','\x0a|❏│\x20\x20\x20_Heure_\x20:\x20','\x0a╭────✧','474428DqGFvq','public','moment-timezone','DD/MM/YYYY'];_0x1a77=function(){return _0x43e061;};return _0x1a77();}const _0x2540d9=_0x4d09;function _0x4d09(_0x38e9ef,_0xb910cb){const _0x1a7777=_0x1a77();return _0x4d09=function(_0x4d09cd,_0x28ee51){_0x4d09cd=_0x4d09cd-0x10b;let _0x57fc67=_0x1a7777[_0x4d09cd];return _0x57fc67;},_0x4d09(_0x38e9ef,_0xb910cb);}(function(_0xadb811,_0x20efa8){const _0x2910a6=_0x4d09,_0x3ee7ef=_0xadb811();while(!![]){try{const _0x1d92de=-parseInt(_0x2910a6(0x141))/0x1*(-parseInt(_0x2910a6(0x10b))/0x2)+parseInt(_0x2910a6(0x10f))/0x3*(-parseInt(_0x2910a6(0x11d))/0x4)+-parseInt(_0x2910a6(0x129))/0x5+parseInt(_0x2910a6(0x113))/0x6*(parseInt(_0x2910a6(0x13d))/0x7)+parseInt(_0x2910a6(0x123))/0x8*(parseInt(_0x2910a6(0x12a))/0x9)+-parseInt(_0x2910a6(0x117))/0xa*(-parseInt(_0x2910a6(0x136))/0xb)+-parseInt(_0x2910a6(0x12b))/0xc;if(_0x1d92de===_0x20efa8)break;else _0x3ee7ef['push'](_0x3ee7ef['shift']());}catch(_0x112c60){_0x3ee7ef['push'](_0x3ee7ef['shift']());}}}(_0x1a77,0xc5858));const util=require(_0x2540d9(0x128)),fs=require(_0x2540d9(0x135)),{zokou}=require(__dirname+_0x2540d9(0x116)),{format}=require(__dirname+_0x2540d9(0x12f)),os=require('os'),moment=require(_0x2540d9(0x11f)),s=require(__dirname+_0x2540d9(0x12d));zokou({'nomCom':_0x2540d9(0x111),'categorie':_0x2540d9(0x114)},async(_0x309e08,_0x50389c,_0x1e1e68)=>{const _0x1fe2de=_0x2540d9;let {ms:_0x7861f6,repondre:_0x4d03a7,prefixe:_0x131c8e,nomAuteurMessage:_0x4ba235,mybotpic:_0x2ef8f4}=_0x1e1e68,{cm:_0xff98ed}=require(__dirname+_0x1fe2de(0x146));var _0x2e8832={},_0x526800=_0x1fe2de(0x11e);s[_0x1fe2de(0x121)][_0x1fe2de(0x138)]()!=_0x1fe2de(0x137)&&(_0x526800=_0x1fe2de(0x122));_0xff98ed['map'](async(_0x4f8e8d,_0x4436e9)=>{const _0x4753f5=_0x1fe2de;if(!_0x2e8832[_0x4f8e8d[_0x4753f5(0x13c)]])_0x2e8832[_0x4f8e8d[_0x4753f5(0x13c)]]=[];_0x2e8832[_0x4f8e8d[_0x4753f5(0x13c)]][_0x4753f5(0x13a)](_0x4f8e8d[_0x4753f5(0x115)]);}),moment['tz'][_0x1fe2de(0x112)]('Etc/GMT');const _0x52269a=moment()[_0x1fe2de(0x13b)](_0x1fe2de(0x10d)),_0x3bb8c7=moment()[_0x1fe2de(0x13b)](_0x1fe2de(0x120));let _0x1d9110=_0x1fe2de(0x11c)+s[_0x1fe2de(0x10c)]+_0x1fe2de(0x10e)+s['PREFIXE']+'\x0a|❏│\x20\x20\x20_Proprio_\x20:\x20'+s[_0x1fe2de(0x119)]+_0x1fe2de(0x132)+_0x526800+_0x1fe2de(0x12c)+_0xff98ed[_0x1fe2de(0x147)]+'\x0a|❏│\x20\x20\x20_Date_\x20:\x20'+_0x3bb8c7+_0x1fe2de(0x11b)+_0x52269a+_0x1fe2de(0x131)+format(os[_0x1fe2de(0x127)]()-os['freemem']())+'/'+format(os[_0x1fe2de(0x127)]())+_0x1fe2de(0x143)+os[_0x1fe2de(0x144)]()+_0x1fe2de(0x124),_0x16a4c9=_0x1fe2de(0x139)+_0x4ba235+_0x1fe2de(0x125);if(_0x1e1e68['categorie']){const _0x41b601=_0x1e1e68['categorie'];if(_0x2e8832[_0x41b601]){_0x16a4c9+='╭────💯'+_0x41b601+_0x1fe2de(0x118);for(const _0x56adc3 of _0x2e8832[_0x41b601]){_0x16a4c9+=_0x1fe2de(0x13e)+_0x56adc3+'*';}_0x16a4c9+=_0x1fe2de(0x12e);}else _0x16a4c9+=_0x1fe2de(0x110)+_0x41b601+_0x1fe2de(0x130);}else for(const _0x4b5248 in _0x2e8832){_0x16a4c9+=_0x1fe2de(0x142)+_0x4b5248+_0x1fe2de(0x118);for(const _0x50e21e of _0x2e8832[_0x4b5248]){_0x16a4c9+=_0x1fe2de(0x13e)+_0x50e21e+'*';}_0x16a4c9+=_0x1fe2de(0x145);}_0x16a4c9+='\x0a◇\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20◇\x0a*»»—————\x20★\x20—————««*\x0a\x22Mettre\x20la\x20commande,\x20et\x20insérez\x20'+_0x131c8e+_0x1fe2de(0x13f);var _0x2c1aa7=_0x2ef8f4();if(_0x2c1aa7[_0x1fe2de(0x126)](/\.(mp4|gif)$/i))try{_0x50389c[_0x1fe2de(0x11a)](_0x309e08,{'video':{'url':_0x2c1aa7},'caption':_0x1d9110+_0x16a4c9,'footer':_0x1fe2de(0x133),'gifPlayback':!![]},{'quoted':_0x7861f6});}catch(_0x167ca6){console[_0x1fe2de(0x140)](_0x1fe2de(0x134)+_0x167ca6),_0x4d03a7(_0x1fe2de(0x134)+_0x167ca6);}else{if(_0x2c1aa7[_0x1fe2de(0x126)](/\.(jpeg|png|jpg)$/i))try{_0x50389c[_0x1fe2de(0x11a)](_0x309e08,{'image':{'url':_0x2c1aa7},'caption':_0x1d9110+_0x16a4c9,'footer':_0x1fe2de(0x133)},{'quoted':_0x7861f6});}catch(_0x2f5d31){console['log'](_0x1fe2de(0x134)+_0x2f5d31),_0x4d03a7(_0x1fe2de(0x134)+_0x2f5d31);}else _0x4d03a7(_0x1d9110+_0x16a4c9);}});
+const util = require('util');
+
+const fs = require('fs-extra');
+
+const { zokou } = require(__dirname + "/../framework/zokou");
+
+const { format } = require(__dirname + "/../framework/mesfonctions");
+
+const os = require("os");
+
+const moment = require("moment-timezone");
+
+const s = require(__dirname + "/../set");
+
+
+
+zokou({ nomCom: "menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
+
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+
+    let { cm } = require(__dirname + "/../framework//zokou");
+
+    var coms = {};
+
+    var mode = "public";
+
+    
+
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+
+        mode = "private";
+
+    }
+
+
+
+
+
+    
+
+
+
+    cm.map(async (com, index) => {
+
+        if (!coms[com.categorie])
+
+            coms[com.categorie] = [];
+
+        coms[com.categorie].push(com.nomCom);
+
+    });
+
+
+
+    moment.tz.setDefault(s.TZ);
+
+
+
+// Créer une date et une heure en GMT
+
+const temps = moment().format('HH:mm:ss');
+
+const date = moment().format('DD/MM/YYYY');
+
+
+
+  let infoMsg =  `
+
+┏━━𒈒JOEL MD NEW UPDATE━━┓
+┃ ⿻Mode: ${mode}
+┃ ⿻User : ${s.OWNER_NAME}
+┃
+┣━━❏joel md-Bot info❏
+┃
+┃ ⿻Library : Baileys
+️┃ ⿻Prefix : ${s.PREFIXE}
+️┃ ⿻Date : ${date}
+┃ ⿻Time : ${temps}
+┃ ⿻Tools : ${cm.length}
+┃ ⿻Ram : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+┃ ⿻Host : ${os.platform()}
+┃
+┣━𒈒JOEL MD V 6.0.0𒈒━➠
+┗━━━𒈒JOEL BUG BOT𒈒━━┛\n\n`;
+
+
+    
+
+let menuMsg = `
+┏━━━━━━━━━━━━━━┓
+┣❏joel md bot 
+┣❏©joel _it🕷️
+┗━━━━━━━━━━━━━━┛\n
+
+
+
+
+𒈒JOEL MD CMDs𒈒
+`;
+
+
+
+    for (const cat in coms) {
+
+        menuMsg += `┏━━━━━⚼ ${cat}`;
+
+        for (const cmd of coms[cat]) {
+
+            menuMsg += `
+┃➠ ${cmd}`;
+
+        }
+
+        menuMsg += `
+┗━━━━━━━━━━━━━━┛\n`
+
+    }
+
+
+
+    menuMsg += `
+
+
+︎┏━━━━━━━━━━━━━━┓
+️┣❏joel md bot 
+┣❏© joel tech 
+┗━━━━━━━━━━━━━━┛\n
+
+
+┏━━━━━━━━━━━━━━┓
+┃➠wa channel: https://whatsapp.com/channel/0029Vade9VgD38CPEnxfYF0M
+┗━━━━━━━━━━━━━━┛\n
+
+
+`;
+
+
+
+   var lien = mybotpic();
+
+
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+
+    try {
+
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *JOEL-BOT*, déveloper joel james" , gifPlayback : true }, { quoted: ms });
+
+    }
+
+    catch (e) {
+
+        console.log("🥵🥵 Menu error " + e);
+
+        repondre("🥵🥵 Menu error " + e);
+
+    }
+
+} 
+
+// Vérification pour .jpeg ou .png
+
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+
+    try {
+
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *TKM-bot*, déveloper cod3uchiha" }, { quoted: ms });
+
+    }
+
+    catch (e) {
+
+        console.log("🥵🥵 Menu error " + e);
+
+        repondre("🥵🥵 Menu error " + e);
+
+    }
+
+} 
+
+else {
+
+    
+
+    repondre(infoMsg + menuMsg);
+
+    
+
+}
+
+
+
+});
+          
