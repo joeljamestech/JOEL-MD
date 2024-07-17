7d19cecfac0f4b5a1a7bba870b531fc987ed52a4
joel tech <joeljamestech3@gmail.com>
2024-07-17 10:39:07 +0300

    Update GroupQuotes.js

--- commandes/GroupQuotes.js
+++ commandes/GroupQuotes.js
@@ -13,13 +13,13 @@ zokou({ nomCom: 'quote', categorie: 'Group' }, async (dest, zk, commandeOptions)
       fetch('https://animechan.xyz/api/random')
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`*HACKING-MD Quotes*
+          repondre(`*JOEL-MD Quotes*
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by *Hacking md*`);
+Powered by *joel md*`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
@@ -37,7 +37,7 @@ Powered by *Hacking md*`);
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by *HACKING-MD*`);
+Powered by *joel tech*`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
