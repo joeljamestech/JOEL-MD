0ed1c5477181a38b84cd969c017ae79e1c6dd118
joel tech <joeljamestech3@gmail.com>
2024-07-20 15:14:31 +0300

    Update uptime.js

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -57,7 +57,7 @@ let linkkk = `https://api.screenshotmachine.com/?key=c04d3a&url=${encodeURICompo
 
 let res = await getBuffer(linkkk);
 
-await zk.sendMessage(dest, { image: res, caption: '𝑾𝒆𝒃 𝑺𝒄𝒓𝒆𝒆𝒏𝒔𝒉𝒐𝒕 𝒃𝒚 * ABROTECH BOT*'}, { quoted: ms });
+await zk.sendMessage(dest, { image: res, caption: '𝑾𝒆𝒃 𝑺𝒄𝒓𝒆𝒆𝒏𝒔𝒉𝒐𝒕 𝒃𝒚 * JOEL BOT*'}, { quoted: ms });
 
 
 }
