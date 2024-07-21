b09308d51e0ec7b3863d0be1306e079ae71adeb1
joel tech <joeljamestech3@gmail.com>
2024-07-21 21:34:17 +0300

    Update menu1.js

--- commandes/menu1.js
+++ commandes/menu1.js
@@ -14,7 +14,7 @@ const s = require(__dirname + "/../set");
 
 
 
-zokou({ nomCom: "bug", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "joel", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
 
     let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
 
