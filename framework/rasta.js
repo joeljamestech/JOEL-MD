d33e223e85c4a95a756b387560cab48ca60c61b4
joel James <165368500+jokathanjoka@users.noreply.github.com>
2024-08-04 08:39:03 +0300

    Update and rename zokou.js to rasta.js

--- framework/rasta.js
+++ framework/rasta.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🥰";
+        infoComs.reaction = "🕷️";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
