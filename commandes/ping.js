3465c4ff16ef202584a8ca04efdbe32148ca7b2c
joel tech <joeljamestech3@gmail.com>
2024-08-08 12:10:29 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "ping", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "ping", reaction: "🕷️", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝐣𝐨𝐞𝐥 𝐦𝐝  \n\n ' + "𝐫𝐞𝐬𝐩𝐨𝐧𝐬𝐞 𝐬𝐩𝐞𝐞𝐝 𝐢𝐬";
+    let z = '𝐣𝐨𝐞𝐥 𝐦𝐝 𝐫𝐞𝐬𝐩𝐨𝐧𝐬𝐞 𝐬𝐩𝐞𝐞𝐝\n\n ' + "𝐢𝐬";
     let d = ' 𝐚𝐛𝐨𝐮𝐭 𝟏𝟎𝟖𝟎.𝟗𝟏 𝐦/𝐬';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/e8311b969d1cb5ce67da8.jpg';
