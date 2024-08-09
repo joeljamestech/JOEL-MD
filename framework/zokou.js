300f58beb8da26f348fb9f9c056de90dad8db924
joel tech <joeljamestech3@gmail.com>
2024-08-09 13:20:31 +0300

    Update zokou.js

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🥰";
+        infoComs.reaction = "❄️";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
