d1d4d20cabecf4969337f3ca888eed9de4cf1964
joel tech <joeljamestech3@gmail.com>
2024-08-11 23:02:06 +0300

    Update menu2.js

--- commandes/menu2.js
+++ commandes/menu2.js
@@ -14,7 +14,7 @@ zokou({ nomCom: "help", categorie: "Menu" }, async (dest, zk, commandeOptions) =
     if (s.MODE_PUBLIC != "oui") {
         mode = "private";
     }
-    var emoji = { "Général": "◈", "Logo": "◈", "hentai": "◈", "weeb": "◈", "Recherche": "◈", "conversion": "◈", "groupe": "◈","Téléchargement":"◈" };
+    var emoji = { "Général": "", "Logo": "", "hentai": "", "weeb": "", "Recherche": "◈", "conversion": "", "groupe": "","Téléchargement":"" };
     cm.map(async (com, index) => { if (!coms[com.categorie])
         coms[com.categorie] = []; coms[com.categorie].push(com.nomCom); });
     const temps = moment(moment()).format("HH:MM:SS");
@@ -22,7 +22,7 @@ zokou({ nomCom: "help", categorie: "Menu" }, async (dest, zk, commandeOptions) =
     const date = moment.tz("asia/karachi").format("DD/MM/YYYY");
     console.log("date" + date);
     console.log("temps " + temps);
-    let menuMsg = "◈◈◈* joel-bot * ◈\n\n";
+    let menuMsg = "\n\n";
     /*menuMsg+=`
     
     
@@ -56,11 +56,11 @@ zokou({ nomCom: "help", categorie: "Menu" }, async (dest, zk, commandeOptions) =
 ╚════--------------- \n\n`;
     for (const cat in coms) {
         if (!emoji[cat]) {
-            emoji[cat] = "◈";
+            emoji[cat] = "";
         }
-        menuMsg += `${emoji[cat]} ◈*${cat} *◈◈${emoji[cat]}\n`;
+        menuMsg += `${emoji[cat]} *${cat} *${emoji[cat]}\n`;
         for (const cmd of coms[cat]) {
-            menuMsg += "\t  ◈◈" + cmd + "" + " \n";
+            menuMsg += "\t  " + cmd + "" + " \n";
         }
     }
     var link = "https://telegra.ph/file/07a6fca837d4d974afb00.jpg";
