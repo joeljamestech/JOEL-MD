34822c46c252a65d23354a609cc8d711af8e0758
joel tech <joeljamestech3@gmail.com>
2024-08-13 08:26:41 +0300

    Update GPT.js

--- commandes/GPT.js
+++ commandes/GPT.js
@@ -8,7 +8,7 @@ Created By joel tech
 Don't claim, okey 
 */
 
-zokou({nomCom:"gpt4",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"bot",reaction:"❄️",categorie:"IA"},async(dest,zk,commandeOptions)=>{
 
   const {repondre,ms,arg}=commandeOptions;
   
