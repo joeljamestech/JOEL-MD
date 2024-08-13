345c7f6e463b2d840f56f8630dec9f6740985d32
joel tech <joeljamestech3@gmail.com>
2024-08-13 08:25:13 +0300

    Update AI.js

--- commandes/AI.js
+++ commandes/AI.js
@@ -1,1 +1,156 @@
-const _0x4a6fc2=_0x1e3c;(function(_0x49b603,_0x47db81){const _0x57aca2=_0x1e3c,_0x3d91bc=_0x49b603();while(!![]){try{const _0x2e69e4=parseInt(_0x57aca2(0x1a7))/0x1*(-parseInt(_0x57aca2(0x1bd))/0x2)+-parseInt(_0x57aca2(0x1aa))/0x3+parseInt(_0x57aca2(0x1a5))/0x4*(-parseInt(_0x57aca2(0x1be))/0x5)+-parseInt(_0x57aca2(0x1a6))/0x6*(parseInt(_0x57aca2(0x1c9))/0x7)+parseInt(_0x57aca2(0x1a1))/0x8*(-parseInt(_0x57aca2(0x19a))/0x9)+parseInt(_0x57aca2(0x19b))/0xa*(-parseInt(_0x57aca2(0x1af))/0xb)+parseInt(_0x57aca2(0x1c6))/0xc;if(_0x2e69e4===_0x47db81)break;else _0x3d91bc['push'](_0x3d91bc['shift']());}catch(_0x17d3c5){_0x3d91bc['push'](_0x3d91bc['shift']());}}}(_0x495f,0x75a3d));const {zokou}=require(_0x4a6fc2(0x1c0)),traduire=require(_0x4a6fc2(0x1c1)),s=require(_0x4a6fc2(0x1b5)),axios=require(_0x4a6fc2(0x1cb));function _0x1e3c(_0x460abb,_0x1edc2c){const _0x495f5a=_0x495f();return _0x1e3c=function(_0x1e3c91,_0x442d8c){_0x1e3c91=_0x1e3c91-0x19a;let _0x287651=_0x495f5a[_0x1e3c91];return _0x287651;},_0x1e3c(_0x460abb,_0x1edc2c);}function _0x495f(){const _0x2b8bc8=['6lXgTdu','56410qCsaat','https://api.maher-zubair.tech/ai/photoleap?q=','../framework/zokou','../framework/traduction','log','Une\x20erreur\x20s\x27est\x20produite','completion','https://api.maher-zubair.tech/ai/mathssolve?q=','39871464lvwSdD','bot','Erreur\x20lors\x20de\x20la\x20requête\x20à\x20BrainShop\x20:','3192vtqyZE','result','axios','cnt','45KibBQF','20VRrlYj','Veuillez\x20entrer\x20les\x20informations\x20nécessaires\x20pour\x20générer\x20l\x27image.','data','Please\x20insert\x20maths\x20calculations\x20like\x201000*2.','dalle','Veuillez\x20poser\x20une\x20questions.','1342072rksEPF','then','how\x20are\x20you\x20joel\x20md\x20is\x20\x27\x20listening\x20to\x20you\x20🕷️.','message','88Dyxnin','156xNGtdl','103031Bdhiyl','error','Erreur:','2282289NkuOkn','http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg=','Erreur\x20lors\x20de\x20la\x20traduction\x20en\x20français\x20:','catch','json','3696319nLHXYC','length','Erreur\x20lors\x20de\x20la\x20requête\x20à\x20BrainShop','*Propulsé\x20par\x20JOEL-MD*','https://api.maher-zubair.tech/ai/chatgpt4?q=','sendMessage','../set','join','status','calcul','gpt','Oups,\x20une\x20erreur\x20est\x20survenue\x20lors\x20du\x20traitement\x20de\x20votre\x20demande.','oupsaa\x20une\x20erreur\x20:\x20','get'];_0x495f=function(){return _0x2b8bc8;};return _0x495f();}zokou({'nomCom':_0x4a6fc2(0x1c7),'reaction':'⚫','categorie':'IA'},async(_0x4cc0a5,_0x326233,_0x3f0bda)=>{const _0x55cbfd=_0x4a6fc2,{repondre:_0x410093,ms:_0x100a63,arg:_0x4fda94}=_0x3f0bda;if(!_0x4fda94||!_0x4fda94[0x0])return _0x410093(_0x55cbfd(0x1a3));try{const _0x48658a=await traduire(_0x4fda94[_0x55cbfd(0x1b6)]('\x20'),{'to':'en'});console[_0x55cbfd(0x1c2)](_0x48658a),fetch(_0x55cbfd(0x1ab)+_0x48658a)[_0x55cbfd(0x1a2)](_0xeaa5e3=>_0xeaa5e3[_0x55cbfd(0x1ae)]())[_0x55cbfd(0x1a2)](_0x5e55b1=>{const _0x3712bd=_0x55cbfd,_0x2800b4=_0x5e55b1['cnt'];console[_0x3712bd(0x1c2)](_0x2800b4),traduire(_0x2800b4,{'to':'fr'})[_0x3712bd(0x1a2)](_0x2e8175=>{_0x410093(_0x2e8175);})['catch'](_0x5f0c6a=>{const _0x2ed7a8=_0x3712bd;console[_0x2ed7a8(0x1a8)](_0x2ed7a8(0x1ac),_0x5f0c6a),_0x410093('Erreur\x20lors\x20de\x20la\x20traduction\x20en\x20français');});})['catch'](_0x40b425=>{const _0x9128a3=_0x55cbfd;console[_0x9128a3(0x1a8)](_0x9128a3(0x1c8),_0x40b425),_0x410093(_0x9128a3(0x1b1));});}catch(_0x2ecb21){_0x410093(_0x55cbfd(0x1bb)+_0x2ecb21);}}),zokou({'nomCom':_0x4a6fc2(0x19f),'reaction':'💎','categorie':'IA'},async(_0x2f7ac3,_0x5688f8,_0x1ef8a5)=>{const _0x1ed787=_0x4a6fc2,{repondre:_0xb444aa,arg:_0xfd7e6,ms:_0x31c8ae}=_0x1ef8a5;try{if(!_0xfd7e6||_0xfd7e6[_0x1ed787(0x1b0)]===0x0)return _0xb444aa(_0x1ed787(0x19c));const _0x4250f2=_0xfd7e6[_0x1ed787(0x1b6)]('\x20'),_0x26eba0=await axios[_0x1ed787(0x1bc)](_0x1ed787(0x1bf)+_0x4250f2),_0x2c3d3f=_0x26eba0[_0x1ed787(0x19d)];let _0x460598=_0x1ed787(0x1b2);if(_0x2c3d3f[_0x1ed787(0x1b7)]&&_0x2c3d3f[_0x1ed787(0x1ca)]){const _0x42ff30=_0x2c3d3f[_0x1ed787(0x1ca)];_0x5688f8[_0x1ed787(0x1b4)](_0x2f7ac3,{'image':{'url':_0x42ff30},'caption':_0x460598},{'quoted':_0x31c8ae});}else _0xb444aa('Erreur\x20lors\x20de\x20la\x20génération\x20de\x20l\x27image');}catch(_0x19e24b){console[_0x1ed787(0x1a8)]('Erreur:',_0x19e24b[_0x1ed787(0x1a4)]||_0x1ed787(0x1c3)),_0xb444aa(_0x1ed787(0x1ba));}}),zokou({'nomCom':_0x4a6fc2(0x1b9),'reaction':'🌐','categorie':'IA'},async(_0xa240d3,_0x430ce8,_0x4d4fa7)=>{const _0x506026=_0x4a6fc2,{repondre:_0x1a7fd0,arg:_0x1e66ad,ms:_0xf937dc}=_0x4d4fa7;try{if(!_0x1e66ad||_0x1e66ad[_0x506026(0x1b0)]===0x0)return _0x1a7fd0(_0x506026(0x1a0));const _0x2fcbc4=_0x1e66ad[_0x506026(0x1b6)]('\x20'),_0x5e0fc1=await axios[_0x506026(0x1bc)](_0x506026(0x1b3)+_0x2fcbc4),_0x436625=_0x5e0fc1[_0x506026(0x19d)];_0x436625?_0x1a7fd0(_0x436625[_0x506026(0x1ca)]):_0x1a7fd0('Erreur\x20lors\x20de\x20la\x20génération\x20de\x20la\x20reponse');}catch(_0x4635c4){console[_0x506026(0x1a8)](_0x506026(0x1a9),_0x4635c4[_0x506026(0x1a4)]||_0x506026(0x1c3)),_0x1a7fd0(_0x506026(0x1ba));}}),zokou({'nomCom':'chat','reaction':'🤔','categorie':'IA'},async(_0x29cf57,_0x3f7834,_0x2a6688)=>{const _0x1882e6=_0x4a6fc2,{repondre:_0x5560cd,ms:_0x423f74,arg:_0x4d0898}=_0x2a6688;if(!_0x4d0898||!_0x4d0898[0x0])return _0x5560cd(_0x1882e6(0x1a3));try{const _0x16bc6a=await traduire(_0x4d0898[_0x1882e6(0x1b6)]('\x20'),{'to':'en'});console[_0x1882e6(0x1c2)](_0x16bc6a),fetch(_0x1882e6(0x1ab)+_0x16bc6a)[_0x1882e6(0x1a2)](_0x35788f=>_0x35788f[_0x1882e6(0x1ae)]())[_0x1882e6(0x1a2)](_0x1e4e4f=>{const _0x49aeeb=_0x1882e6,_0x1d26d8=_0x1e4e4f[_0x49aeeb(0x1cc)];console['log'](_0x1d26d8),traduire(_0x1d26d8,{'to':'en'})[_0x49aeeb(0x1a2)](_0x41c229=>{_0x5560cd(_0x41c229);})['catch'](_0x5abd5d=>{const _0x51a1ad=_0x49aeeb;console[_0x51a1ad(0x1a8)](_0x51a1ad(0x1ac),_0x5abd5d),_0x5560cd('Erreur\x20lors\x20de\x20la\x20traduction\x20en\x20français');});})[_0x1882e6(0x1ad)](_0x32d6f5=>{const _0x594e37=_0x1882e6;console[_0x594e37(0x1a8)](_0x594e37(0x1c8),_0x32d6f5),_0x5560cd(_0x594e37(0x1b1));});}catch(_0x2b5ea6){_0x5560cd(_0x1882e6(0x1bb)+_0x2b5ea6);}}),zokou({'nomCom':_0x4a6fc2(0x1b8),'reaction':'😂','categorie':'IA'},async(_0x3a6b01,_0x3b0f62,_0x272acf)=>{const _0x33f9ae=_0x4a6fc2,{repondre:_0x3f9176,arg:_0x242a68,ms:_0x34512f}=_0x272acf;if(!_0x242a68||_0x242a68[_0x33f9ae(0x1b0)]===0x0)return _0x3f9176(_0x33f9ae(0x19e));const _0x9ecab9=await fetch(_0x33f9ae(0x1c5)+cal),_0x1da7b7=await _0x9ecab9[_0x33f9ae(0x1ae)]();await _0x3f9176(_0x1da7b7[_0x33f9ae(0x1ca)]),console[_0x33f9ae(0x1c2)](_0x1da7b7[_0x33f9ae(0x1c4)]);});
+const { zokou } = require('../framework/zokou');
+const traduire = require("../framework/traduction") ;
+const { default: axios } = require('axios');
+const text2prompt = require('../framework/text2prompt')
+const { ai } = require('../framework/mesfonctions')
+const { react } = require('../framework/utils')
+const conf = require('../set');
+
+
+
+
+zokou({nomCom:"bot",reaction:"❄️",categorie:"Ai"},async(dest,zk,commandeOptions)=>{
+
+  const {repondre,ms,arg}=commandeOptions;
+  
+    if(!arg || !arg[0])
+    {return repondre("yes I'm listening to you.")}
+    //var quest = arg.join(' ');
+  try{
+    
+    
+const message = await traduire(arg.join(' '),{ to : 'en'});
+ console.log(message)
+fetch(`http://api.brainshop.ai/get?bid=182418&key=UQXAO1yyrPLRnhf6&uid=[uid]&msg=${message}`)
+.then(response => response.json())
+.then(data => {
+  const botResponse = data.cnt;
+  console.log(botResponse);
+
+  traduire(botResponse, { to: 'en' })
+    .then(translatedResponse => {
+      repondre(translatedResponse);
+    })
+    .catch(error => {
+      console.error('Error when translating into French :', error);
+      repondre('Error when translating into French');
+    });
+})
+.catch(error => {
+  console.error('Error requesting BrainShop :', error);
+  repondre('Error requesting BrainShop');
+});
+
+  }catch(e){ repondre("oops an error : "+e)}
+    
+  
+  });  
+
+
+
+  zokou({ nomCom: "dalle", reaction: "📡", categorie: "Ai" }, async (dest, zk, commandeOptions) => {
+    const { repondre, arg, ms } = commandeOptions;
+  
+    try {
+      if (!arg || arg.length === 0) {
+        return repondre(`Please enter the necessary information to generate the image.`);
+      }
+  
+      // Regrouper les arguments en une seule chaîne séparée par "-"
+      const image = arg.join(' ');
+      const response = await axios.get(`http://api.maher-zubair.tech/ai/photoleap?q=${image}`);
+      
+      const data = response.data;
+      let caption = '┃powered by ⬡〘TKM md〙⬡┃';
+      
+      if (data.status == 200) {
+        // Utiliser les données retournées par le service
+        const imageUrl = data.result;
+        zk.sendMessage(dest, { image: { url: imageUrl }, caption: caption }, { quoted: ms });
+      } else {
+        repondre("Error during image generation.");
+      }
+    } catch (error) {
+      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+      repondre("Oops, an error occurred while processing your request");
+    }
+  });
+  
+  zokou({ nomCom: "gpt", reaction: "📡", categorie: "Ai" }, async (dest, zk, commandeOptions) => {
+    const { repondre, arg, ms } = commandeOptions;
+  
+    try {
+      if (!arg || arg.length === 0) {
+        return repondre(`Please ask a question.`);
+      }
+  
+      // Regrouper les arguments en une seule chaîne séparée par "-"
+      const question = arg.join(' ');
+      const response = await axios.get(`http://api.maher-zubair.tech/ai/chatgpt4?q=${question}`);
+      
+      const data = response.data;
+      if (data) {
+        repondre(data.result);
+      } else {
+        repondre("Error during response generation.");
+      }
+    } catch (error) {
+      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+      repondre("Oops, an error occurred while processing your request.");
+    }
+  });
+
+zokou(
+  {
+    nomCom: 'gpt4',
+    reaction: '📡',
+    alias: ['chatgpt4'],
+    categorie: 'Ai'
+  },
+  
+  async (dest, zk, commandeOptions) => {
+    const {ms, arg, repondre} = commandeOptions;
+    if (!arg[0]) 
+     return await repondre('ask something');
+    const msg = await zk.sendMessage(dest,{text: 'thinking......'},{quoted: ms});
+    res = await ai(arg.join(' '));
+    if (res.status === 200) {
+      await zk.sendMessage(dest, {text: res.reply, edit: msg.key}, {quoted: ms});
+      await react(dest, zk, ms, '🤖');
+    } else {
+      await zk.sendMessage(dest, {text: 'an error occred generating resopnce', edit: msg.key}, {quoted: ms});
+      await react(dest, zk, ms, '⚠️');
+    }
+  }
+  )
+
+zokou(
+  {
+    nomCom:"text2prompt",
+    reaction:"📡",
+    categorie:"Ai"
+  },
+  
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre} = commandeOptions;
+    
+    if (!arg[0])
+      return await repondre(`text argument is required \n> try ${conf.PREFIXE}text2prompt a sad cat`)
+      
+    const text = await traduire(arg.join(' '), { to: 'en'} );
+    
+    await text2prompt(text).then(sus).catch(err)
+    
+    function sus(res) {
+      if(res.status)
+        return repondre(res.prompt)
+      else
+        repondre('an error occoured genrating prompt')
+    }
+    function err(e){
+      console.log(`an error occoured at :${e}`)
+      return repondre('an error occoured genrating prompt')
+    }
+    
+  }
+);
