562cc8fc8ec7f8f99645b53653b2cdc6887d97cb
joel tech <joeljamestech3@gmail.com>
2024-08-14 23:08:45 +0300

    Update and rename quote.js to quote.js

--- commandes/quote.js
+++ commandes/quote.js
@@ -13,14 +13,14 @@ zokou({ nomCom: 'quote', categorie: 'Fun' }, async (dest, zk, commandeOptions) =
         .then((response) => response.json())
         .then(async (quote) => {
           repondre(`╔══════════════════════════╗
-║   Hacking-md               ║
+║  joel-md               ║
 ╚══════════════════════════╝
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by Hacking-MD`);
+Powered by joel-it`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
@@ -33,14 +33,14 @@ Powered by Hacking-MD`);
         .then((response) => response.json())
         .then(async (quote) => {
           repondre(`╔══════════════════════════╗
-║   Hacking-md               ║
+║   joel-md               ║
 ╚══════════════════════════╝
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by Hacking-MD`);
+Powered by joel-it`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
