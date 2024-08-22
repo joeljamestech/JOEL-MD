05f5f425d5c22c6b934cc64a67801ee825c3056c
𝚓𝚘𝚎𝚕 𝚓𝚊𝚖𝚎𝚜 <joeljamestech3@gmail.com>
2024-08-23 00:06:03 +0300

    Update tyl.js

--- commandes/tyl.js
+++ commandes/tyl.js
@@ -16,7 +16,7 @@ Helpers - @joel james
         - @joel it
 WHATSAPP - 255714595078
 SUPPORT GROUP - https://wa.me/255714595078
-YOUTUBE - http://youtube.com/joeljames tech
+Don't change this info else bot won't work by joeljames tech
 
 */
 
@@ -29,4 +29,60 @@ YOUTUBE - http://youtube.com/joeljames tech
 
 
 
-z
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { getBuffer } = require("../framework/dl/Function");
+const { default: axios } = require('axios');
+
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
+
+
+zokou({ nomCom: 'play',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🎼', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`click to search and listen musics https://www.msn.com/en-us/music`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'update',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🎞️', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`_*joel md bot is running on its latest vision*_`) 
+
+   
+
+
+  }
+);
