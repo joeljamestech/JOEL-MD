936b8061c66ee72acd47819368a06bb3d891e411
𝚓𝚘𝚎𝚕 𝚓𝚊𝚖𝚎𝚜 <joeljamestech3@gmail.com>
2024-08-25 11:19:06 +0300

    Update Answer.js

--- commandes/Answer.js
+++ commandes/Answer.js
@@ -45,7 +45,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
 
 
 
-  zokou({ nomCom: "bing", reaction: "🌀", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+  zokou({ nomCom: "dalle", reaction: "🌀", categorie: "IA" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
   
     try {
@@ -58,7 +58,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
       const response = await axios.get(`http://api.maher-zubair.tech/ai/photoleap?q=${image}`);
       
       const data = response.data;
-      let caption = '*bing images by joel tech*';
+      let caption = '*image by joel tech*';
       
       if (data.status == 200) {
         // Utiliser les données retournées par le service
