741e996ed1638c0fe74ef827bb1eebb68cc39359
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-08-26 10:38:38 +0300

    Update conversion.js

--- commandes/conversion.js
+++ commandes/conversion.js
@@ -252,7 +252,7 @@ zokou({ nomCom: "write", categorie: "Conversion", reaction: "👨🏿‍💻" },
     // Create the sticker
     const stickerMess = new Sticker(meme, {
       pack: nomAuteurMessage,
-      author: 'Hacking-Md',
+      author: 'Joel-Md',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
