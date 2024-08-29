67d8a232d915e957601da8894235634286d72a97
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-08-29 07:06:07 +0300

    Create getvcf.js

---
+++ commandes/getvcf.js
@@ -0,0 +1,100 @@
+/*
+░█ 
+░░░░░██╗░█████╗░███████╗██╗░░░░░
+░░░░░██║██╔══██╗██╔════╝██║░░░░░
+░░░░░██║██║░░██║█████╗░░██║░░░░░
+██╗░░██║██║░░██║██╔══╝░░██║░░░░░
+╚█████╔╝╚█████╔╝███████╗███████╗
+░╚════╝░░╚════╝░╚══════╝╚══════╝
+
+███╗░░░███╗██████╗░
+████╗░████║██╔══██╗
+██╔████╔██║██║░░██║
+██║╚██╔╝██║██║░░██║
+██║░╚═╝░██║██████╔╝
+╚═╝░░░░░╚═╝╚═════╝░
+
+██████╗░░█████╗░████████╗
+██╔══██╗██╔══██╗╚══██╔══╝
+██████╦╝██║░░██║░░░██║░░░
+██╔══██╗██║░░██║░░░██║░░░
+██████╦╝╚█████╔╝░░░██║░░░
+╚═════╝░░╚════╝░░░░╚═╝░░░
+WHATSAPP BOT BY @joel james tech
+Helpers - @joel james
+        - @joel it
+WHATSAPP - 255714595078
+SUPPORT GROUP - https://wa.me/255714595078
+Don't change this info else bot won't work by joeljames tech
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { getBuffer } = require("../framework/dl/Function");
+const { default: axios } = require('axios');
+
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
+
+
+zokou({ nomCom: 'vcf',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '📄', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_please wait..._*`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'getallmembers',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🐞', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_getting all members_*`) 
+
+   
+
+
+  }
+);
+
+
