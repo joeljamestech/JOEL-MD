a7a41cb5a9f094c23ffb810b8c3bf85e5bf659f9
𝚓𝚘𝚎𝚕 𝚓𝚊𝚖𝚎𝚜 <joeljamestech3@gmail.com>
2024-09-22 11:14:37 +0300

    𝙻𝚘𝚛𝚍 𝚓𝚘𝚎𝚕

--- lordjoel/Reboot.js
+++ lordjoel/Reboot.js
@@ -6,7 +6,7 @@ const {zokou}=require("../framework/zokou")
 
 
 
-zokou({nomCom:"restart",categorie:"Mods",reaction:"♻️"},async(dest,z,com)=>{
+zokou({nomCom:"restart",categorie:"Mods",reaction:"📴"},async(dest,z,com)=>{
 
 
   
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("joel md bot Restarting ♻️");
+    repondre("JOEL-MD bot Restarting ⏳");
 
   exec("pm2 restart all");
   
