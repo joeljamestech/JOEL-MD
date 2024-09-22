a67823dcace747b93bb43638bb504fc43dfa24d2
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-09-22 07:40:55 +0300

    lord joel

--- lordjoel/sc.js
+++ lordjoel/sc.js
@@ -4,7 +4,7 @@ const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "sc", catégorie:"Général", reaction: "✨", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
   const githubRepo = 'https://api.github.com/repos/joeljamestech/JOEL-MD';
-  const img = 'https://telegra.ph/file/07a6fca837d4d974afb00.jpg';
+  const img = 'https://files.catbox.moe/5a2euh.jpg';
 
   try {
     const response = await fetch(githubRepo);
