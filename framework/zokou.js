db519e41238a8f83d33297c62ae4c32cbccfdc5e
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-09-22 11:47:10 +0300

    Lord joel

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🥰";
+        infoComs.reaction = "❒";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
