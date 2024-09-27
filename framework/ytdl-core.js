db92420157748a4ebb03b0fc6b77887f15f6736a
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-09-27 09:55:36 +0300

    𝙻𝚘𝚛𝚍 𝚓𝚘𝚎𝚕

--- framework/ytdl-core.js
+++
@@ -1,1 +0,0 @@
-l','ytdl-core','6DERfpV','3912020Ouvwun','12838378JJhScC','videoId','1919958dZtqWP','4344028OqrUkK','downloadFromInfo','892765NqGbJI','chooseFormat','yt-search','12037095TIFpBi','exports','videos','5alpetW','2BMRwif','error','timestamp','title','format
