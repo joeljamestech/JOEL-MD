e2b11b0d4c0d7703c4206c8e2af84df0912f9f44
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-09-30 15:32:35 +0300

    𝙻𝚘𝚛𝚍 𝚓𝚘𝚎𝚕

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&dVnLFSpN",
+  SESSION_ID: process.env.SESSION_ID || "",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
