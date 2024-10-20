42b2ddd086ac975e6a94c2bab638e8b1963efc1e
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-10-20 19:42:00 +0300

    Create joelchnl.js

---
+++ src/plugin/joelchnl.js
@@ -0,0 +1,24 @@
+const ping = async (m, sock) => {
+  const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const text = m.body.slice(prefix.length + cmd.length).trim();
+
+  if (cmd === "support") {
+    const start = new Date().getTime();
+    await m.React('❀');
+    const end = new Date().getTime();
+    const responseTime = (end - start) / 1000;
+
+    const text = `┏━━━━❐
+┃
+┃
+┃
+┃
+┃
+┃
+┗━━━━━━━━━━━━━━━━━━━━━━━❑`;
+    sock.sendMessage(m.from, { text }, { quoted: m });
+  }
+}
+
+export default ping;
