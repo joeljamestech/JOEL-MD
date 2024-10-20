ada0956320398854e31e43e725c02c78ae9eb013
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-10-20 21:42:42 +0300

    ᴊᴏᴇʟ ᴍᴇ ʙᴏᴛ

--- src/plugin/joelchnl.js
+++ src/plugin/joelchnl.js
@@ -1,35 +1,112 @@
-const ping = async (m, sock) => {
+import config from '../../config.cjs';
+import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
+import Jimp from 'jimp';
+const { generateWAMessageFromContent, proto } = pkg;
+
+const alive = async (m, Matrix) => {
+  const uptimeSeconds = process.uptime();
+  const days = Math.floor(uptimeSeconds / (3600 * 24));
+  const hours = Math.floor((uptimeSeconds % (3600 * 24)) / 3600);
+  const minutes = Math.floor((uptimeSeconds % 3600) / 60);
+  const seconds = Math.floor(uptimeSeconds % 60);
+  const timeString = `${String(days).padStart(2, '0')}-${String(hours).padStart(2, '0')}-${String(minutes).padStart(2, '0')}-${String(seconds).padStart(2, '0')}`;
   const prefix = config.PREFIX;
-const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
-const text = m.body.slice(prefix.length + cmd.length).trim();
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const text = m.body.slice(prefix.length + cmd.length).trim();
 
-  if (cmd === "support") {
-    const start = new Date().getTime();
-    await m.React('❀');
-    const end = new Date().getTime();
-    const responseTime = (end - start) / 1000;
-
-    const text = `┏━━━━❐
-┃『ᴊᴏᴇʟ ᴍᴇ ʙᴏᴛ』
-┗━━━━
+  if (['channel', 'support', 'bot'].includes(cmd)) {
+    const width = 800;
+    const height = 500;
+    const image = new Jimp(width, height, 'black');
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
+    const textMetrics = Jimp.measureText(font, timeString);
+    const textHeight = Jimp.measureTextHeight(font, timeString, width);
+    const x = (width / 2) - (textMetrics / 2);
+    const y = (height / 2) - (textHeight / 2);
+    image.print(font, x, y, timeString, width, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
+    const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
+    
+    const uptimeMessage = `┏━━━━❐
+┃『ᴊᴏᴇʟ ᴍᴅ ʙᴏᴛ』
+┗
 ┏
 ┃『ᴡᴀ ᴄʜᴀɴɴᴇʟ』
 ┃❑ https://whatsapp.com/channel/0029Vade9VgD38CPEnxfYF0M
-┗━━━━━━━━
+┗
 ┏
 ┃『ᴡᴀ ɢʀᴏᴜᴘ 』
 ┃❐ https://chat.whatsapp.com/GxlcVwkXrPo2YwNoQI5TwS
-┗━━━━━━━━━━━
+┗
 ┏
 ┃『ʏᴏᴜ ᴛᴜʙᴇ 』
 ┃❐https://youtube.com/@joeltech255?si=rqhYlAhFtqK7CVX2
-┗━━━━━━━━━━━━━
+┗
 ┏
 ┃『ᴛɪᴋᴛᴏᴋ 』
 ┃❐https://www.tiktok.com/@joeljamestech
-┗━━━━━━━━━━━━━━━━━━━━━━━❑`;
-    sock.sendMessage(m.from, { text }, { quoted: m });
-  }
-}
+┗━━━━━━━━━━━━━━━━━━❑
+ᴋᴇᴇᴘ ᴜsɪɴɢ ᴊᴏᴇʟ ᴍᴅ ʙᴏᴛ`;
+    
+    const buttons = [
+      {
+        "name": "quick_reply",
+        "buttonParamsJson": JSON.stringify({
+          display_text: "SCRIPT",
+          id: `${prefix}repo`
+        })
+      },
+      {
+        "name": "quick_reply",
+        "buttonParamsJson": JSON.stringify({
+          display_text: "PING",
+          id: `${prefix}ping`
+        })
+      }
+    ];
 
-export default ping;
+    const msg = generateWAMessageFromContent(m.from, {
+      viewOnceMessage: {
+        message: {
+          messageContextInfo: {
+            deviceListMetadata: {},
+            deviceListMetadataVersion: 2
+          },
+          interactiveMessage: proto.Message.InteractiveMessage.create({
+            body: proto.Message.InteractiveMessage.Body.create({
+              text: uptimeMessage
+            }),
+            footer: proto.Message.InteractiveMessage.Footer.create({
+              text: "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴊᴏᴇʟ ᴋᴀɴɢ'ᴏᴍᴀ"
+            }),
+            header: proto.Message.InteractiveMessage.Header.create({
+              ...(await prepareWAMessageMedia({ image: buffer }, { upload: Matrix.waUploadToServer })),
+              title: ``,
+              gifPlayback: false,
+              subtitle: "",
+              hasMediaAttachment: false
+            }),
+            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+              buttons
+            }),
+            contextInfo: {
+              quotedMessage: m.message,
+              forwardingScore: 999,
+              isForwarded: true,
+              forwardedNewsletterMessageInfo: {
+                newsletterJid: '255714595078@s.whatsapp.net',
+                newsletterName: "JOel",
+                serverMessageId: 143
+              }
+            }
+          }),
+        },
+      },
+    }, {});
+
+    await Matrix.relayMessage(msg.key.remoteJid, msg.message, {
+      messageId: msg.key.id
+    });
+  }
+};
+
+export default alive;
