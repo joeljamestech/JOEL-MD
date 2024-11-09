6dabf9cd6dab09ba571cfbe9e9497779be2f0bc1
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-11-09 10:30:14 +0300

    ᴊᴏᴇʟ ᴊᴀᴍᴇs ᴛᴇᴄʜ

--- config.cjs
+++ config.cjs
@@ -5,7 +5,8 @@ require("dotenv").config();
 const config = {
   SESSION_ID: process.env.SESSION_ID || "Your Session Id",
   PREFIX: process.env.PREFIX || '.',
-  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true,
+  AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true,
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
   AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
