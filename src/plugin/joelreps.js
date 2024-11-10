2fa4cfbb8ba395f92b748c704fdfcf61f082934b
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-11-10 10:09:18 +0300

    ᴊᴏᴇʟ ᴊᴀᴍᴇs ᴛᴇᴄʜ

--- src/plugin/joelreps.js
+++ src/plugin/joelreps.js
@@ -12,14 +12,14 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
     const responseTime = (end - start) / 1000;
 
     const text = `┏━❐
-┃ 𝙹𝙾𝚎𝚕 𝚖𝚍 𝚋𝚘𝚝
-┃ 𝚟𝚒𝚜𝚒𝚘𝚗 (𝟼) 
-┗━━━━━
-   ┃
-  ┏
-  ┃https://github.com/joeljamestech/JOEL-MD
-  ┃𝚖𝚊𝚍𝚎 𝚋𝚢 𝙹𝙾𝚎𝚕 𝚝𝚎𝚌𝚑
-  ┗━━━━━━━━━
+┃۞𝙹𝙾𝚎𝚕 𝚖𝚍 𝚋𝚘𝚝
+┃۞𝚟𝚒𝚜𝚒𝚘𝚗 (𝟼) 
+┗━━━━━❑
+┏━❐
+┃https://github.com/joeljamestech/JOEL-MD
+┃𝚖𝚊𝚍𝚎 𝚋𝚢 𝙹𝙾𝚎𝚕 𝚝𝚎𝚌𝚑
+┗━━━━━━━━━❑
+┏━━━━━❏
 ┃https://whatsapp.com/channel/0029Vade9VgD38CPEnxfYF0M
 ┗━━━━━━━━━━━━━━━━❑`;
     sock.sendMessage(m.from, { text }, { quoted: m });
