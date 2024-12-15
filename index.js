4754d523386fc2095a0da7ac1170f921595ba6a4
Marisel <shenapam3@gmail.com>
2024-12-15 22:17:56 +0300

    Joel Md

--- index.js
+++ index.js
@@ -30612,7 +30612,7 @@ async function downloadSessionData() {
         console.error('Please add your session to SESSION_ID env !!');
         return false;
     }
-    const sessdata = config.SESSION_ID.split("Mercedes&")[1];
+    const sessdata = config.SESSION_ID.split("Joel-Md&")[1];
     const url = `https://pastebin.com/raw/${sessdata}`;
     try {
         const response = await axios.get(url);
